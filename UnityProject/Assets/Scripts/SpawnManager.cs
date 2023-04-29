0807122c37758b7b78c82d1235b4de96cf5c3b11
Lenovo Legion 5 - Dawid <dawka_wody@wp.pl>
2023-04-29 16:16:33 +0200

    Dodano spawner zombie i chodzenie ich za graczem

--- UnityProject/Assets/Scripts/SpawnManager.cs
+++ UnityProject/Assets/Scripts/SpawnManager.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject _orcPrefab;
+    [SerializeField]
+    private float _spawnRate;
+
+    private bool spawn = true;
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start(){
+        StartCoroutine(ZombieSpawnCo());
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update(){
         
+    }
+
+    IEnumerator ZombieSpawnCo(){
+        while (spawn){
+            yield return new WaitForSeconds(_spawnRate);
+            Instantiate(_orcPrefab, transform.position + new Vector3(Random.Range(32, -32), Random.Range(27, -26), 0), transform.rotation);
+        }
     }
 }
--- UnityProject/Assets/Scripts/Zombie.cs
+++ UnityProject/Assets/Scripts/Zombie.cs
@@ -5,19 +5,34 @@ using UnityEngine;
 public class Zombie : MonoBehaviour
 {
     [SerializeField]
-    private GameObject player;
-    [SerializeField]
-    private Rigidbody2D _rigidbody;
-    [SerializeField]
     private float _speed;
+
+    private GameObject _player;
     // Start is called before the first frame update
     void Start(){
-        
+        _player = GameObject.Find("Player");
     }
 
     // Update is called once per frame
     void Update(){
-        Vector2 toMove = player.transform.position - transform.position;
-        _rigidbody.velocity = new Vector2(Mathf.Clamp(_speed, 0, toMove.x), Mathf.Clamp(_speed, 0, toMove.y));
+        transform.Translate(CalculateMovement() * _speed * Time.deltaTime);
+    }
+
+    Vector2 CalculateMovement(){
+        Vector2 toMove = _player.transform.position - transform.position;
+        if (toMove.x < 0){
+            toMove.x = -1;
+        }
+        else if (toMove.x > 0){
+            toMove.x = 1;
+        }
+        if (toMove.y < 0){
+            toMove.y = -1;
+        }
+        else if (toMove.y > 0){
+            toMove.y = 1;
+        }
+
+        return toMove;
     }
 }
