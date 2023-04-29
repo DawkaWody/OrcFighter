050b9eed57f17cea3d0d872e477e4c9f6da5b8c2
Lenovo Legion 5 - Dawid <dawka_wody@wp.pl>
2023-04-29 15:12:22 +0200

    Dodano działanie portalu
    Obiekt Portal przyjmuje w edytorze animator, czas trwania animacji wejścia oraz nową scenę do załadowania. Po wejściu do portalu wykonuje on animację, czeka i ładuje nową scenę

--- UnityProject/Assets/Scripts/Portal.cs
+++ UnityProject/Assets/Scripts/Portal.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Portal : MonoBehaviour
 {
     [SerializeField]
     private Animator _animator;
+    [SerializeField]
+    private float _portalEnterDuration;
+    [SerializeField]
+    private string _sceneToLoad;
     // Start is called before the first frame update
     void Start(){
         
@@ -19,6 +24,13 @@ public class Portal : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.CompareTag("Player")){
             _animator.SetTrigger("portalEnter");
+            StartCoroutine(PortalEnterCo(other.gameObject));
         }
+    }
+
+    IEnumerator PortalEnterCo(GameObject player){
+        player.SetActive(false);
+        yield return new WaitForSeconds(_portalEnterDuration + 2f);
+        SceneManager.LoadScene(_sceneToLoad);
     }
 }
